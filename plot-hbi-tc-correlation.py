153eb5df77656c0b73e12f91992e79a3b691515f
Jordan Ashley <jashley6@vols.utk.edu>
2025-07-23 16:09:17 -0700

    :sparkles: Plot HBI vs TC measurement correlations

---
+++ plot-hbi-tc-correlation.py
@@ -0,0 +1,45 @@
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+
+serial_num   = '20USBHX2002885'
+
+hbi_gain_away   = np.load('hbi_gain_away.npy')
+hbi_gain_under  = np.load('hbi_gain_under.npy')
+hbi_innse_away  = np.load('hbi_innse_away.npy')
+hbi_innse_under = np.load('hbi_innse_under.npy')
+
+tc_gain_away   = np.load('tc_gain_away.npy')
+tc_gain_under  = np.load('tc_gain_under.npy')
+tc_innse_away  = np.load('tc_innse_away.npy')
+tc_innse_under = np.load('tc_innse_under.npy')
+
+def flatten_run(run):
+    '''Flatten ABC lists into one list of all channels per run'''
+    return [val for chip in run for val in chip]
+
+def plot_measurement(hbi_data, tc_data, measurement_name, stream):
+    plt.figure()
+    num_runs = len(hbi_data)
+    norm     = Normalize(vmin=0, vmax=num_runs - 1)
+    cmap     = plt.get_cmap("Greens")
+    
+    plt.figure(figsize=(10, 5))
+
+    for run_num, (hbi_run, tc_run) in enumerate(zip(hbi_data, tc_data)):
+        hbi_flat = flatten_run(hbi_run)
+        tc_flat  = flatten_run(tc_run)
+        color    = cmap(norm(run_num))
+        plt.scatter(hbi_flat, tc_flat, color=color, label=f"Run {run_num}", s=10)
+
+    plt.title(f"{serial_num} {measurement_name}, {stream} Stream HBI vs TC Correlation")
+    plt.xlabel(f"HBI {measurement_name}")
+    plt.ylabel(f"TC {measurement_name}")
+    plt.grid()
+
+    plt.savefig(f"{serial_num}-{measurement_name}-{stream}-correlation.pdf")
+
+plot_measurement(hbi_innse_away, tc_innse_away, 'noise', 'away')
+plot_measurement(hbi_innse_under, tc_innse_under, 'noise', 'under')
