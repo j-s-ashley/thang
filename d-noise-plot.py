9a98949694fbb5b0ea1633c99b4ad14d83877e46
Jordan Ashley <jashley6@vols.utk.edu>
2025-07-30 18:10:45 -0700

    :sparkles: Average over each ASIC

--- d-noise-plot.py
+++ d-noise-plot.py
@@ -6,21 +6,21 @@ from matplotlib.colors import Normalize
 
 serial_num   = '20USBHX2002884' # used for naming
 
-# --- Load Data --- #
-hbi_3pg_innse_away       = np.load('hbi_3pg_innse_away.npy')
-hbi_10pg_innse_away      = np.load('hbi_10pg_innse_away.npy')
-hbi_3pg_innse_under      = np.load('hbi_3pg_innse_under.npy')
-hbi_10pg_innse_under     = np.load('hbi_10pg_innse_under.npy')
+# --- Load Data and Get ASIC Mean Values --- #
+hbi_3pg_innse_away       = np.load('hbi_3pg_innse_away.npy').mean(axis=1)
+hbi_10pg_innse_away      = np.load('hbi_10pg_innse_away.npy').mean(axis=1)
+hbi_3pg_innse_under      = np.load('hbi_3pg_innse_under.npy').mean(axis=1)
+hbi_10pg_innse_under     = np.load('hbi_10pg_innse_under.npy').mean(axis=1)
 
-tc_warm_3pg_innse_away   = np.load('tc_warm_3pg_innse_away.npy')
-tc_warm_10pg_innse_away  = np.load('tc_warm_10pg_innse_away.npy')
-tc_warm_3pg_innse_under  = np.load('tc_warm_3pg_innse_under.npy')
-tc_warm_10pg_innse_under = np.load('tc_warm_10pg_innse_under.npy')
+tc_warm_3pg_innse_away   = np.load('tc_warm_3pg_innse_away.npy').mean(axis=1)
+tc_warm_10pg_innse_away  = np.load('tc_warm_10pg_innse_away.npy').mean(axis=1)
+tc_warm_3pg_innse_under  = np.load('tc_warm_3pg_innse_under.npy').mean(axis=1)
+tc_warm_10pg_innse_under = np.load('tc_warm_10pg_innse_under.npy').mean(axis=1)
 
-tc_cold_3pg_innse_away   = np.load('tc_cold_3pg_innse_away.npy')
-tc_cold_10pg_innse_away  = np.load('tc_cold_10pg_innse_away.npy')
-tc_cold_3pg_innse_under  = np.load('tc_cold_3pg_innse_under.npy')
-tc_cold_10pg_innse_under = np.load('tc_cold_10pg_innse_under.npy')
+tc_cold_3pg_innse_away   = np.load('tc_cold_3pg_innse_away.npy').mean(axis=1)
+tc_cold_10pg_innse_away  = np.load('tc_cold_10pg_innse_away.npy').mean(axis=1)
+tc_cold_3pg_innse_under  = np.load('tc_cold_3pg_innse_under.npy').mean(axis=1)
+tc_cold_10pg_innse_under = np.load('tc_cold_10pg_innse_under.npy').mean(axis=1)
 
 # --- Get Differences --- #
 # --- Since data is passed as NumPy objects, operations can be done directly
@@ -36,13 +36,12 @@ diff_cold_10pg_under = tc_cold_10pg_innse_under - hbi_10pg_innse_under
 
 # --- Plot Stuff --- #
 def plot_measurement(y_data, stream, y_ID):
-    channel_labels = np.repeat(np.arange(10), 128)
-    cmap           = plt.cm.get_cmap("tab10")
+    channel_labels = np.repeat(np.arange(10))
     
     plt.figure(figsize=(10, 5))
     
-    x_data  = np.arange(len(diff_data)) # Should come out to 1280
-    x_ID    = 'channel'
+    x_data  = np.arange(len(diff_data)) # Should come out to 10
+    x_ID    = 'ASIC'
     measurement_name = 'TC_HBI_difference'
     scatter = plt.scatter(x_data, y_data, c=channel_labels, cmap=cmap)
     
